ad850073884a6eead375de163f77856eb81b1c96
Abhishek Khadkathoki <143115079+akhadkathoki@users.noreply.github.com>
2026-04-24 03:36:14 +0530

    Update xai_utils.py

--- utils/xai_utils.py
+++ utils/xai_utils.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 from torchvision import transforms
-from grad_cam import GradCAM
+from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 
 device = torch.device("cpu")
